2c594fae80985cd6accc488adee5ced19590d0ce
michael <michael.ampm@gmail.com>
2021-09-18 11:15:13 -0700

    fix

--- dep/lib/circleengine/src/lib.rs
+++ dep/lib/circleengine/src/lib.rs
@@ -1,4 +1,8 @@
+use std::collections::HashMap;
+
 #[no_mangle]
-pub extern "C" fn circleprocess() {
-    println!("Hello world!");
+pub extern "C" fn circleprocess(sample: f32) {
+  let s: HashMap<i32>;
+  println!("Hello world! {sample}", sample);
+  let arr = [1,2,234];
 }

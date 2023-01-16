fcfb33d5f9dd60c60f7762266a526a6463159900
michael <michael.ampm@gmail.com>
2023-01-16 14:00:39 -0800

    remove unused modules

--- src/kokopellivcv.cpp
+++ src/kokopellivcv.cpp
@@ -1,9 +1,9 @@
 #include "kokopellivcv.hpp"
 
 #include "modules/Circle.hpp"
-#include "modules/Aion.hpp"
-#include "modules/Member.hpp"
-#include "modules/Ether.hpp"
+// #include "modules/Aion.hpp"
+// #include "modules/Member.hpp"
+// #include "modules/Ether.hpp"
 
 Plugin *pluginInstance;
 
@@ -11,7 +11,7 @@ void init(rack::Plugin *p) {
 	pluginInstance = p;
 
   p->addModel(modelCircle);
-  p->addModel(modelAion);
-  p->addModel(modelMember);
-  p->addModel(modelEther);
+  // p->addModel(modelAion);
+  // p->addModel(modelMember);
+  // p->addModel(modelEther);
 }
